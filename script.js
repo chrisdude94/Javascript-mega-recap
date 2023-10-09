1aebe36562c095fd6b1107bae9985456fd6fcee9
chrisdude94 <142973331+chrisdude94@users.noreply.github.com>
2023-10-09 14:17:11 +0200

    *

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1: Konsoll logg ""Hello World!"
+
+let hello = ("hello world");
+
+console.log(hello);
+
+// 1.2:  Definer to variabler som er to forskjellige tall. 
+//Definer de slik at det skal være mulig å sette en ny verdi til variablene senere. Konsoll log hva verdiene er slik: "variabel1 = VERDI1" og "variabel2 = VERDI2" 
+
+let variabel1 = 5;
+let variabel2 = 10;
+
+console.log("variabel1 = " + variabel1)
+console.log("variabel2 = " + variabel2)
+
+// 1.3: Konsoll log dette: "Hello world! The sum of my two variables is:  SUM"
+
+let sum = variabel1 + variabel2;
+
+console.log("Hello world! The sum of my two varuables is: " + (sum));
+
+// 1.4:  Øk verdien på den første variabelen med en. Ikke bruk tall for å få til dette.....hvilke operator kombinasjon kan man bruke for å få til dette? Konsoll log hva verdien er nå
+
+variabel1++;
+
+console.log(variabel1);
+
+// 1.5:  Reduser verdien på den andre variabelen med en. Ikke bruk tall for å få til dette.....
+//hvilke operator kombinasjon kan man bruke for å få til dette? Konsoll log hva verdien er nå
+
+variabel2--;
+
+console.log(variabel2)
+
+
+
+
+
+ 
